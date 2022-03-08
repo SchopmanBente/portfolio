b46c2f231c2f0bc5f1c7bc2c760d530259e64eec
Bente Schopman <b.l.schopman@gmail.com>
2022-03-08 16:21:41 +0100

    Safe delete of forms.py at app_landingspage

--- portfolio/app_landingpage/admin.py
+++ portfolio/app_landingpage/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import *
-from .forms import *
 # Register your models here.
 from django.contrib import admin
 from app_landingpage.models import LandingPage
