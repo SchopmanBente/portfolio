415d033cb046c436fc96d9cffdcffa71d7786182
Bente Schopman <b.l.schopman@gmail.com>
2022-03-29 18:05:47 +0200

    Improved JS add now a div @portfolio

--- portfolio/static/scripts/script.js
+++ portfolio/static/scripts/script.js
@@ -1,21 +1,4 @@
 function showFilters() {
-  document.getElementById("filter").innerHTML +=  "<h3>This is the text which has been inserted by JS</h3>";
+  document.getElementById("filter").innerHTML +=  "<div class='filters'></div>";
 
-}
-
-function addRow() {
-  const div = document.createElement('div');
-
-  div.className = 'row';
-
-  div.innerHTML = `"<nav role='filter'>Test </nav>'" `;
-
-  document.getElementById('content').appendChild(div);
-}
-
-function removeRow(input) {
-  document.getElementById('content').removeChild(input.parentNode);
-}
-function basicPopup(url) {
-    popupWindow = window.open(url,'popUpWindow','height=500,width=500,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
 }
